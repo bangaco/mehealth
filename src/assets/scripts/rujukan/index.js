90c454bd6b0240af465e39e159b0ff6463a2b60b
unknown <febriansyah200207@gmail.com>
2024-10-29 15:43:54 +0700

    feat: add chart rujukan

---
+++ src/assets/scripts/rujukan/index.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-undef */
+const ctx = document.getElementById('myChart');
+const ctc = document.getElementById('rujukan');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Poli Gigi', 'Poli Psikology', 'Poli Umum'],
+    datasets: [{
+      label: 'Pasangan',
+      data: [12, 19, 3],
+      borderWidth: 1,
+    },
+    {
+      label: 'Pekerja',
+      data: [7, 10, 5],
+      borderWidth: 1,
+    },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+}
+);
+
+new Chart(ctc, {
+  type: 'line',
+  data: {
+    labels: ['January', 'February', 'March', 'April'],
+    datasets: [{
+      label: 'Rs Awas Bos Group',
+      data: [1, 5, 2, 0],
+      borderWidth: 1,
+      fill: false,
+      tension: 0.1,
+    },
+    {
+      label: 'RS Syafira Pekanbaru',
+      data: [2, 3, 0, 1],
+      borderWidth: 1,
+      fill: false,
+      tension: 0.1,
+    },
+    {
+      label :'RS. Pertamedika Ummi Rosnati',
+      data: [1, 0, 0, 9],
+      borderWidth: 1,
+      fill: false,
+      tension: 0.1,
+    },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+}
+);
